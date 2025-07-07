6c7dc46426dcd6adfb168ba210949acbb3dd310c
hoeltz <for.imam@gmail.com>
2025-07-07 16:21:58 +0700

    Fix Vite configuration to resolve main.tsx loading error

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname),
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
